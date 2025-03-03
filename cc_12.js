03a9c0a7403b2f1ef087dccfab64b8dbb3daa622
Emma Adey <eadey@usf.edu>
2025-03-02 23:09:10 -0500

    Task 3 - Implemented Dynamic Inventory List

--- cc_12.js
+++ cc_12.js
@@ -13,12 +13,46 @@ revenueCard.innerHTML =  // Populating the Revenue metric card
 
 dashboard1.appendChild(revenueCard);
 
-// Task 2:  Updating Dashboard Metrics – Working with NodeLists and Arrays
-const metricCards = document.querySelectorAll(`.metric-card`); //selecting all elements with the class "metr"ic card"
-const metricCardsArray = Array.from(metricCards); //converting NodeList to an array
+// Task 2: Updating Dashboard Metrics – Working with NodeLists and Arrays
+const metricCards = document.querySelectorAll(`.metric-card`); // selecting all elements with the class "metr"ic card"
+const metricCardsArray = Array.from(metricCards); // Convert the NodeList into an array using Array.from()
 
 metricCardsArray.forEach((card) => {
     card.innerText += ` - Updated`; //"- Updated" to text in card
-    card.style.backgroundColor = `lightpink`; //changing background color
+    card.style.backgroundColor = `lightpink`; //changing background color of card
 });
+
+//Task 3 - Implemented Dynamic Inventory List
+const inventoryList = document.getElementById("inventoryList");
+
+function addInventoryItem(product) { // Function to add a new product item to the inventory list
+    let newLi = document.createElement("li"); 
+    newLi.setAttribute("class","product-item"); 
+    newLi.setAttribute("dataset",product); 
+    newLi.addEventListener("click", () => {removeInventoryItem(newLi)}); 
+    newLi.textContent = product; 
+    inventoryList.appendChild(newLi); 
+};
+
+addInventoryItem("Click Below!"); //Adding inventory item (click below!)
+addInventoryItem("Choose Item"); // Adding inventory item (choose item)
+
+let productForm = document.getElementById('productForm');
+let error = document.getElementById('error');
+
+productForm.addEventListener('submit', (event) => { // Adding an event listener for a click of the submit button
+    let productName = document.getElementById('productName').value; // Storing the content of the productName text input
+    if (productName.trim() === '') { // Checking if the input is empty
+        error.textContent = 'Product Name Error'; // Error message
+        event.preventDefault(); // Prevent form submission
+    } else {
+        error.textContent = '';
+        addInventoryItem(productName); // Calling addInventoryItem()
+        event.preventDefault(); 
+    }
+});
+
+function removeInventoryItem(item) { 
+    inventoryList.removeChild(item); //Use removeChild within the inventory list to remove the clicked item.
+};
 
